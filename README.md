3f5235dba5af1ffec00fa69f1a52c03874df0b92
mim <max.efimow@gmail.com>
2024-06-01 14:01:09 +0200

    terraform init

--- README.md
+++ README.md
@@ -1,23 +1,83 @@
 # Requirements
-aws - account
+aws Account 
+
 aws-cli, kubectl, helm on local machine
 
 # Installing infra
 
 Init and apply terraform. main.tf terraform for eks + api gateway
 
-check if kubectl get svc works
+go to infra folder
+
+Terraform init
+```
+terraform init
+```
+
+installing eks + api gateway
+```
+terraform apply
+```
+
+Check if eks was installed and kubectl works  
+```
+aws eks update-kubeconfig --region eu-central-1 --name my-eks-cluster
+kubectl get svc -n kube-system
+```
+
 
 ## install ingress controller
+
+```
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx --set controller.publishService.enabled=true
+```
+
+
+
+
 
 ## key generation
 keytool -genkeypair -alias mykey -keyalg RSA -keystore mykeystore.p12 -storetype PKCS12 -validity 365 -keysize 2048
 
 ### creating secret on eks
 kubectl create secret generic my-keystore-secret --from-file=mykeystore.p12
+
+
+## bulding image
+
+Use gradle to build spring-boot jar
+
+Navigate to docker folder and execute
+
+```
+docker build -t my-spring-boot-app -f Dockerfile ..
+```
+
+## pushing image to aws repo
+
+# login to docker->ecr
+
+```
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin <YOUR_AWS_ACCOUNT_ID>.dkr.ecr.eu-central-1.amazonaws.com
+```
+
+creating repo for images
+
+```
+aws ecr create-repository --repository-name my-app-repo
+```
+
+image tag
+```
+docker tag demo-gw:latest <aws_account_id>.dkr.ecr.<your-region>.amazonaws.com/my-app-repo:latest
+```
+
+docker push
+```
+docker push <aws_account_id>.dkr.ecr.<your-region>.amazonaws.com/my-app-repo:latest
+```
 
 
 # Getting Started
---
+++ infra/main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "eks" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name = "eks-subnet-${count.index}"
+  }
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy
+  ]
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks_cluster_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks_node_group_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-nodes"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = aws_subnet.eks[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  instance_types = ["t3.micro"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_node_group_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks_node_group_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.eks_node_group_AmazonEKS_CNI_Policy
+  ]
+}
